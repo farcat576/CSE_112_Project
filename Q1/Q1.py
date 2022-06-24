a477c452f1e2d7157f99d83c07f481e6d20a05bd
Farhan Ali <96176384+farcat576@users.noreply.github.com>
2022-06-24 18:32:45 +0530

    Refactor

--- Q1/Q1.py
+++ Q1/Q1.py
@@ -10,14 +10,18 @@ assert len(commands) <= 256, "Too many instructions!"
 
 #assertion statements for each instruction
 #mov + last bit later
+
+#Opcode mapping
 opcode = {"add":["10000",'A'], "sub":["10001","A"], "mov":["1001",""], "ld":["10100","D"],
         "st":["10101","D"],"mul":["10110","A"],"div":["10111","C"],"rs":["11000","B"],
         "ls":["11001","B"],"xor":["11010","A"],"or":["11011","A"],"and":["11100","A"],
         "not":["11101","C"],"cmp":["11110","C"],"jmp":["11111","E"],"jlt":["01100","E"],
         "jgt":["01101","E"],"je":["01111","E"],"hlt":["01010","F"]}
 
+#Register mapping
 reg={'R0':'000','R1':'001','R2':'010','R3':'011','R4':'100','R5':'101','R6':'110','FLAGS':'111'}
 
+#Conversion functions for each type
 def type_A(reg1,reg2,reg3):
     return '00'+reg[reg1]+reg[reg2]+reg[reg3]
 
@@ -36,6 +40,7 @@ def type_E(memory):
 def halt():
     return '0000000000'
 
+#Helper functions for checking instruction parts
 def reg_check(register):
     return register in reg
 
@@ -46,13 +51,19 @@ def imm_check(imm):
         return 0
 
 def flags_check(register):
-    return register == "FLAGS":
+    return register == "FLAGS"
 
+def var_check(var):
+    return var in var_dict
+
+def label_check(lab):
+    return lab in label_dict
+
+#Functions to check the validity of instructions according to type
 def A_check(line):
     if len(line) != 4:
         return 0
     return (line[1] in reg) and (line[2] in reg) and (line[3] in reg)
-
 
 def B_check(line):
     if len(line) != 3:
@@ -67,28 +78,53 @@ def C_check(line):
 def D_check(line):
     if len(line) != 3:
         return 0
-    return reg_check(line[1]) and (line[2] in var_dict)
+    return reg_check(line[1]) and var_check(line[2])
 
 def E_check(line):
     if len(line) != 2:
         return 0
-    return reg_check(line[1])
+    return label_check(line[1])
 
 def F_check(line):
     return line==["hlt"]
 
-var_init = filter(lambda x: x[:-1]=='var ' and len(x.split())==2,data)
-lim=len(data)
-var_dict={var_init[i][4]:str(lim+i) for i in range(len(var_init))}
+# Filtering out statements
+def parse(data):
+    data=[line.split() for line in data]
+    
+    var_start=True
+    var_dict=dict()
+    label_dict=dict()
+    op_dict=dict()
+    err_dict=dict()
+    lim=len(data)
+    
+    for i in range(lim):
+        line=data[i]
+        if (len(line)==2 and line[0]=="var"):
+            var_dict[line[1]]=bin(lim)[2:].rjust(8,'0')
+            lim+=1
+        elif (len(line)==1 and line[-1][-1]==":"):
+            label_dict[line[-1][:-1]=bin(lim)[2:].rjust(8,'0')
+        elif (line[0] in opcode):
+            op_dict[str(i)]=line
+        else:
+            err_dict[str(i)]=" ".join(line)
+                       
+    return var_dict,label_dict,op_dict
+            
+#var_init = filter(lambda x: x[:-1]=='var ' and len(x.split())==2,data)
+#lim=len(data)
+#var_dict={var_init[i][4]:bin(lim+i)[2:].rjust(8,'0') for i in range(len(var_init))}
 
-label_trav = [(data[i],i) for i in range(len(data)) if data[i][-1]==':' and len(data[i].split())==1]
-label_init= [elem[0] for elem in label_trav]
-label_dict={label_init[i][0][:-1]:str(label_init[i][1]) for i in range(len(label_trav))}
+#label_trav = [(data[i],i) for i in range(len(data)) if data[i][-1]==':' and len(data[i].split())==1]
+#label_init= [elem[0] for elem in label_trav]
+#label_dict={label_init[i][0][:-1]:bin(label_init[i][1])[2:].rjust(8,'0') for i in range(len(label_trav))}
 
-op_init = filter(lambda x: x.split()[0] in opcode,data)
+#op_init = filter(lambda x: x.split()[0] in opcode,data)
 
-err_init =filter(lambda x: (x not in var_init) and (x not in label_init) and (x not in label_init),data)
-assert len(err_init) == 0
+#err_init =filter(lambda x: (x not in var_init) and (x not in label_init) and (x not in label_init),data)
+#assert len(err_init) == 0
 
 L=[]
 for line in op_init:
@@ -130,7 +166,4 @@ with open('binary.txt' ,'w') as f:
     f.write(L)
 #
 #Check var and label again
-#Fix binary string widths
-#Mix all code together
-#Change parameters of writing parts
 #Handle asserts
