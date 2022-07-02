8e311ec9c4b10b029484ad7f9a345c66b52f30c0
Farhan Ali <96176384+farcat576@users.noreply.github.com>
2022-07-02 16:14:52 +0530

    Type hinting done

--- Q1/Q1.py
+++ Q1/Q1.py
@@ -1,9 +1,10 @@
 import sys
+from typing import TextIO
 
 #Creating a list of strings of the given input and storing it in a variable commands
 with open("q1testcase.txt", 'r') as f:
-    data = f.readlines()
-    commands = [data[i].strip() for i in range(len(data))] 
+    text = f.readlines()
+    commands = [text[i].strip() for i in range(len(text))]
     commands = list(filter(lambda a: a != "", commands))
 
 output = open('binary.txt',"w")
@@ -13,32 +14,34 @@ L = []
 opcode = {"add": ["10000", 'A'], "sub": ["10001", "A"], "mov": ["1001", ""], "ld": ["10100", "D"],
           "st": ["10101", "D"], "mul": ["10110", "A"], "div": ["10111", "C"], "rs": ["11000", "B"],
           "ls": ["11001", "B"], "xor": ["11010", "A"], "or": ["11011", "A"], "and": ["11100", "A"],
-          "not": ["11101", "C"], "cmp": ["11110", "C"], "jmp": ["11111", "E"], "jlt": ["01100", "E"],
-          "jgt": ["01101", "E"], "je": ["01111", "E"], "hlt": ["01010", "F"]}
+          "not": ["11101", "C"], "cmp": ["11110", "C"], "jmp": ["11111", "E"],
+          "jlt": ["01100", "E"], "jgt": ["01101", "E"], "je": ["01111", "E"], "hlt": ["01010", "F"]}
 
 #creating error_dict which contains code-error mapping and is called in error() function
 error_dict = {"101": "duplicate hlt statement detected.", "102": "Last instruction is not hlt.",
               "103": "Too many instructions given.", "201": "Register not found",
-              "202": "Immediate value format not recognised", "203": "Illegal use of FLAGS register detected.",
+              "202": "Immediate value format not recognised",
+              "203": "Illegal use of FLAGS register detected.",
               "204": "Syntax Error.", "301": "Variable not defined",
               "302": "Variable not defined at the beginning", "303": "Label not found.",
               "304": "Syntax error in use of variable.", "305": "Syntax error in use of label.",
               "306": "Variable used as label.", "307": "Label used as variable.",
-              "308": "Variable already initialised before.", "309": "Label already initialised before."
+              "308": "Variable already initialised before.",
+              "309": "Label already initialised before."
               }
 
-def error(error_code, error_line = '-1'):
+def error(error_code: str, error_line: str = '-1') -> None:
     """Universal program for checking errors"""
     if error_line == '-1':
         output.write(error_dict[error_code] + "\nAssembling halted." )
         output.close()
         sys.exit()  #Ending the program incase an error is detected
     output.write("At line " + error_line + ', ' + error_dict[error_code] + "\nAssembling halted.")
-    output.close() 
+    output.close()
     sys.exit() #Ending the program incase an error is detected
 
 # Filtering out statements
-def parse(data):
+def parse(data: list[str]) -> tuple[ dict[str,str], dict[str,str], dict[str,list[str]]]:
     data = [line.split() for line in data]
 
     var_start = True #Declaring var_start for handling the position of variable declaration in input
@@ -54,8 +57,8 @@ def parse(data):
     for i in range(lim):
         line = data[i]
         #checks if variable is in the start and if variable is legally identified
-        if (line[0] == "var"): 
-            if (len(line)!=2):
+        if line[0] == "var": 
+            if len(line) != 2:
                 error("304", str(i + 1))
             if not var_start:
                 error("302", str(i + 1))
@@ -63,37 +66,37 @@ def parse(data):
                 error("308", str(i + 1))
             var_dict[line[1]] = lim
             lim += 1
-        #checks for a colon in the line, if present then check conditions for a label and store line in label_dict
-        elif (": " in " ".join(line)): 
-            if (line[0][-1] != ":"):
+        #checks for a colon in the line, if present then check conditions for label and stores in label_dict
+        elif ": " in " ".join(line):
+            if line[0][-1] != ":":
                 error("305", str(i + 1))
             if line[0][:-1] in label_dict:
                 error("309", str(i + 1))
             label_dict[line[0][:-1]] = i
             line = line[1:]
-            if (line[0] in opcode):
+            if line[0] in opcode:
                 op_dict[str(i)] = line
             else:
                 error("204", str(i + 1))
             var_start = False
         #checking if the line is in opcode and appending to op_dict for assembling
-        elif (line[0] in opcode):
+        elif line[0] in opcode:
             op_dict[str(i)] = line
             var_start = False
         else:
             error("204", str(i + 1))
-    
+
     lin=list(op_dict.values()).count(["hlt"])
     if lin == 0:
         error('102')
     elif lin > 1:
         error("101", str(data.index(['hlt']) + 1))
-    
+
     #variable declaration do not take up code space in the memory
     offset = len(var_dict)
     var_dict = {key : bin(var_dict[key]-offset)[2:].rjust(8, '0')
                 for key in var_dict}
-    label_dict = {key : bin(label_dict[key]-offset)[2:].rjust(8, '0') 
+    label_dict = {key : bin(label_dict[key]-offset)[2:].rjust(8, '0')
                 for key in label_dict}
     op_dict = {str(int(key) - offset) : op_dict[key]
                 for key in op_dict}
@@ -103,59 +106,60 @@ def parse(data):
 var_dict, label_dict, op_dict = parse(commands)
 
 # Register mapping
-reg = {'R0': '000', 'R1': '001', 'R2': '010', 'R3': '011', 'R4': '100', 'R5': '101', 'R6': '110', 'FLAGS': '111'}
+reg = {'R0': '000', 'R1': '001', 'R2': '010', 'R3': '011',
+       'R4': '100', 'R5': '101', 'R6': '110', 'FLAGS': '111'}
 
 # Conversion functions for each type
-def type_A(reg1, reg2, reg3):
+def type_A(reg1: str, reg2: str, reg3: str) -> str:
     return '00' + reg[reg1] + reg[reg2] + reg[reg3]
 
 
-def type_B(reg1, imm):
+def type_B(reg1: str, imm: str) -> str:
     return reg[reg1] + bin(int(imm))[2:].rjust(8, '0')
 
 
-def type_C(reg1, reg2):
+def type_C(reg1: str, reg2: str) -> str:
     return '00000' + reg[reg1] + reg[reg2]
 
 
-def type_D(reg1, memory):
+def type_D(reg1: str, memory: str) -> str:
     return reg[reg1] + var_dict[memory]
 
 
-def type_E(memory):
+def type_E(memory: str) -> str:
     return '000' + label_dict[memory]
 
 
-def halt():
+def halt() -> str:
     return '00000000000'
 
 
 # Helper functions for checking instruction parts
-def reg_check(register):
+def reg_check(register: str) -> bool:
     return register in reg and register != "FLAGS"
 
 #checking if format of given immediate value is valid and is in the range 0-255(both inclusive)
-def imm_check(imm):
+def imm_check(imm: str) -> bool:
     if imm[0] == '$' and imm[1:].isdigit():
         return int(imm[1:]) >= 0 and int(imm[1:]) < 256
     else:
         return False
 
 # Boolean functions that check whether the given parameter holds true or not
-def flags_check(register):
+def flags_check(register: str) -> bool:
     return register == "FLAGS"
 
 
-def var_check(var):
+def var_check(var: str) -> bool:
     return var in var_dict
 
 
-def label_check(lab):
+def label_check(lab: str) -> bool:
     return lab in label_dict
 
 
 # Functions to check the validity of instructions according to type
-def A_check(line, error_ln):
+def A_check(line: list[str], error_ln: str) -> bool:
     if len(line) != 4:
         error("204",error_ln)
     elif not ((reg_check(line[1])) and (reg_check(line[2])) and (reg_check(line[3]))):
@@ -167,19 +171,19 @@ def A_check(line, error_ln):
 
 
 #immediate value is not checked, pls fix
-def B_check(line,error_ln):
+def B_check(line: str, error_ln: str) -> bool:
     if len(line) != 3:
         error("204", error_ln)
-    elif not (reg_check(line[1])):
-        if (flags_check(line[1])):
+    elif not reg_check(line[1]):
+        if flags_check(line[1]):
             error("203", error_ln)
         error("201", error_ln)
-    elif not (imm_check(line[2])):
+    elif not imm_check(line[2]):
         error("202",str(int(error_ln)))
     return True
 
 
-def C_check(line,error_ln):
+def C_check(line: str, error_ln: str) -> bool:
     if len(line) != 3:
         error("204", error_ln)
     if not (reg_check(line[1]) and reg_check(line[2])):
@@ -187,35 +191,35 @@ def C_check(line,error_ln):
     return True
 
 
-def D_check(line,error_ln):
+def D_check(line: str, error_ln: str) -> bool:
     if len(line) != 3:
         error("204", error_ln)
-    if not (reg_check(line[1])):
-        if (flags_check(line[1])):
+    if not reg_check(line[1]):
+        if flags_check(line[1]):
             error("203", error_ln)
         error("201", error_ln)
-    if not (var_check(line[2])):
+    if not var_check(line[2]):
         if label_check(line[2]):
             error("307", error_ln)
         error("301", error_ln)
     return True
 
 
-def E_check(line,error_ln):
+def E_check(line: str, error_ln: str) -> bool:
     if len(line) != 2:
         error("204", error_ln)
-    if not (label_check(line[1])) :
+    if not label_check(line[1]) :
         if var_check(line[2]):
             error("306", error_ln)
         error("303", error_ln)
     return True
 
-def F_check(line,error_ln):
+def F_check(line: str, error_ln: str) -> bool:
     if line != ["hlt"]:
         error("204", error_ln)
     return True
 
-def process():
+def process() -> list[str]:
     for num in op_dict:
         line = op_dict[num]
         oper = line[0]
@@ -226,7 +230,7 @@ def process():
                 error("204", str(int(num) + 1))
             elif not reg_check(line[1]):
                 error("201", str(int(num) + 1))
-            
+
             if imm_check(line[2]):
                 op += "0"
                 type = "B"
@@ -259,14 +263,13 @@ def process():
 
     return L
 
-def main(output):
-    L = process()
+def main(output: TextIO) -> None:
+    out = process()
     #with open('binary.txt', 'w') as f:
-    for i in range(len(L)-1):
-        output.write(L[i] + '\n')
+    for i in range(len(out)-1):
+        output.write(out[i] + '\n')
 
-    output.write(L[len(L)-1])
+    output.write(out[len(out)-1])
 
 main(output)
-
 #Comments done till label_check
